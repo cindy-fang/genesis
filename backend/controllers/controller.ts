101498ae04e46569ea68b10167387fcc70cb5992
Waheeb Barqawi <trolltrolltrolllollollol@outlook.com>
2024-03-31 01:47:45 -0400

    changed from one option to multiple goal options

--- backend/controllers/controller.ts
+++ backend/controllers/controller.ts
@@ -28,7 +28,7 @@ export async function questionQuery(req: Request, res: Response) {
     2. The season is ${input.season}
     3. The size of the future garden is ${input.size}
     4. Their gardening experience is ${input.experience}
-    5. Their gardening goal is ${input.goal}
+    5. Their gardening goals is/are ${input.goal.join(", ")}
     6. Their time commitment is ${input.timeCommitment} a day
     With that in mind, generate the following data in this exact json format (no text no response except json):
     [{ name: string, dailytodolist: ["...", "...", "...", "...", "..."], description: string}]
@@ -41,7 +41,6 @@ export async function questionQuery(req: Request, res: Response) {
     for (const plant of plants) {
       plant["image"] = await getImage("cartoon " + plant.name);
     }
-    // console.log(plants);
     return res.json(plants);
   } catch (e) {
     if (e instanceof z.ZodError) {
